a9b59f06f0fb760d4b96246bbfc75ac6e5355c12
Jean luc Tuyishime <jeanluctuyishime@Jeans-MacBook-Pro.local>
2020-03-22 17:10:44 +0200

    add pagination functionality

--- src/pages/View/VaccinatedChildren.js
+++ src/pages/View/VaccinatedChildren.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { Header, Icon, Table, Container, Message } from 'semantic-ui-react';
+import { Header, Icon, Table, Container, Message, Pagination } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import moment from 'moment';
@@ -13,12 +13,18 @@ import '../../styles/register.css';
 class VaccinatedChildren extends Component {
 
     state = {
+        page: 1,
+        itemsPerPage: 7,
         vaccinatedChildren: []
     }
 
     componentDidMount = () => {
         const { getVaccinationChildren } = this.props;
         getVaccinationChildren();
+    };
+
+    setPageNum = (event, { activePage }) => {
+        this.setState({ page: activePage });
     };
 
     UNSAFE_componentWillReceiveProps = (nextProps) => {
@@ -31,6 +37,13 @@ class VaccinatedChildren extends Component {
 
     render() {
         const { vaccinatedChildren } = this.state
+        const itemsPerPage = 7;
+        const { page } = this.state;
+        const totalPages = vaccinatedChildren.length / itemsPerPage;
+        const items = vaccinatedChildren.slice(
+            (page - 1) * itemsPerPage,
+            (page - 1) * itemsPerPage + itemsPerPage
+        );
         return (
             <>
                 <MenuBar />
@@ -39,46 +52,57 @@ class VaccinatedChildren extends Component {
                     <Header.Content className="children-text">Vaccinated Children</Header.Content>
                 </Header>
                 <Container>
-                    {vaccinatedChildren.length ? <Table singleLine>
-                        <Table.Header>
-                            <Table.Row>
-                                <Table.HeaderCell>
-                                    id
+                    {vaccinatedChildren.length ?
+                        <>
+                            <Table singleLine>
+                                <Table.Header>
+                                    <Table.Row>
+                                        <Table.HeaderCell>
+                                            id
                            </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    User Id
+                                        <Table.HeaderCell>
+                                            User Id
                             </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    firstName
+                                        <Table.HeaderCell>
+                                            firstName
                               </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    lastName
+                                        <Table.HeaderCell>
+                                            lastName
                               </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    birth
+                                        <Table.HeaderCell>
+                                            birth
                               </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    sex
+                                        <Table.HeaderCell>
+                                            sex
                               </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    created At
+                                        <Table.HeaderCell>
+                                            created At
                               </Table.HeaderCell>
-                            </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                            {_.map(vaccinatedChildren, ({ id, userId, firstName, lastName, birth, sex, createdAt }) => (
-                                <Table.Row key={id}>
-                                    <Table.Cell>{id}</Table.Cell>
-                                    <Table.Cell>{userId}</Table.Cell>
-                                    <Table.Cell>{firstName}</Table.Cell>
-                                    <Table.Cell>{lastName}</Table.Cell>
-                                    <Table.Cell>{moment(birth).format("dddd, MMMM Do YYYY")}</Table.Cell>
-                                    <Table.Cell>{sex}</Table.Cell>
-                                    <Table.Cell>{moment(createdAt).format("dddd, MMMM Do YYYY")}</Table.Cell>
-                                </Table.Row>
-                            ))}
-                        </Table.Body>
-                    </Table> :
+                                    </Table.Row>
+                                </Table.Header>
+                                <Table.Body>
+                                    {_.map(items, ({ id, userId, firstName, lastName, birth, sex, createdAt }) => (
+                                        <Table.Row key={id}>
+                                            <Table.Cell>{id}</Table.Cell>
+                                            <Table.Cell>{userId}</Table.Cell>
+                                            <Table.Cell>{firstName}</Table.Cell>
+                                            <Table.Cell>{lastName}</Table.Cell>
+                                            <Table.Cell>{moment(birth).format("dddd, MMMM Do YYYY")}</Table.Cell>
+                                            <Table.Cell>{sex}</Table.Cell>
+                                            <Table.Cell>{moment(createdAt).format("dddd, MMMM Do YYYY")}</Table.Cell>
+                                        </Table.Row>
+                                    ))}
+                                </Table.Body>
+                            </Table>
+                            <div className="center-pagination">
+                                <Pagination
+                                    activePage={page}
+                                    totalPages={totalPages}
+                                    siblingRange={1}
+                                    onPageChange={this.setPageNum}
+                                />
+                            </div>
+                        </> :
                         <Message info>
                             <p className="center-text">No Vaccinated children for now...</p>
                         </Message>}
--- src/pages/View/ViewParents.js
+++ src/pages/View/ViewParents.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { Header, Icon, Table, Container, Message } from 'semantic-ui-react';
+import { Header, Icon, Table, Container, Message, Pagination } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import moment from 'moment';
@@ -13,6 +13,8 @@ import '../../styles/register.css';
 class ViewParents extends Component {
 
     state = {
+        page: 1,
+        itemsPerPage: 7,
         parents: []
     }
 
@@ -20,6 +22,10 @@ class ViewParents extends Component {
         console.log(this.props);
         const { get } = this.props;
         get();
+    };
+
+    setPageNum = (event, { activePage }) => {
+        this.setState({ page: activePage });
     };
 
     UNSAFE_componentWillReceiveProps = (nextProps) => {
@@ -32,6 +38,13 @@ class ViewParents extends Component {
 
     render() {
         const { parents } = this.state
+        const itemsPerPage = 7;
+        const { page } = this.state;
+        const totalPages = parents.length / itemsPerPage;
+        const items = parents.slice(
+            (page - 1) * itemsPerPage,
+            (page - 1) * itemsPerPage + itemsPerPage
+        );
         return (
             <>
                 <MenuBar />
@@ -40,46 +53,57 @@ class ViewParents extends Component {
                     <Header.Content>Parents</Header.Content>
                 </Header>
                 <Container>
-                    {parents.length ? <Table singleLine>
-                        <Table.Header>
-                            <Table.Row>
-                                <Table.HeaderCell>
-                                    id
+                    {parents.length ?
+                        <>
+                            <Table singleLine>
+                                <Table.Header>
+                                    <Table.Row>
+                                        <Table.HeaderCell>
+                                            id
                            </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    userId
+                                        <Table.HeaderCell>
+                                            userId
                             </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    firstName
+                                        <Table.HeaderCell>
+                                            firstName
                               </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    lastName
+                                        <Table.HeaderCell>
+                                            lastName
                               </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    email
+                                        <Table.HeaderCell>
+                                            email
                               </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    Phone
+                                        <Table.HeaderCell>
+                                            Phone
                               </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    createdAt
+                                        <Table.HeaderCell>
+                                            createdAt
                               </Table.HeaderCell>
-                            </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                            {_.map(parents, ({ id, userId, firstName, lastName, email, phone, createdAt }) => (
-                                <Table.Row key={id}>
-                                    <Table.Cell>{id}</Table.Cell>
-                                    <Table.Cell>{userId}</Table.Cell>
-                                    <Table.Cell>{firstName}</Table.Cell>
-                                    <Table.Cell>{lastName}</Table.Cell>
-                                    <Table.Cell>{email}</Table.Cell>
-                                    <Table.Cell>{phone}</Table.Cell>
-                                    <Table.Cell>{moment(createdAt).format("dddd, MMMM Do YYYY")}</Table.Cell>
-                                </Table.Row>
-                            ))}
-                        </Table.Body>
-                    </Table> :
+                                    </Table.Row>
+                                </Table.Header>
+                                <Table.Body>
+                                    {_.map(items, ({ id, userId, firstName, lastName, email, phone, createdAt }) => (
+                                        <Table.Row key={id}>
+                                            <Table.Cell>{id}</Table.Cell>
+                                            <Table.Cell>{userId}</Table.Cell>
+                                            <Table.Cell>{firstName}</Table.Cell>
+                                            <Table.Cell>{lastName}</Table.Cell>
+                                            <Table.Cell>{email}</Table.Cell>
+                                            <Table.Cell>{phone}</Table.Cell>
+                                            <Table.Cell>{moment(createdAt).format("dddd, MMMM Do YYYY")}</Table.Cell>
+                                        </Table.Row>
+                                    ))}
+                                </Table.Body>
+                            </Table>
+                            <div className="center-pagination">
+                                <Pagination
+                                    activePage={page}
+                                    totalPages={totalPages}
+                                    siblingRange={1}
+                                    onPageChange={this.setPageNum}
+                                />
+                            </div>
+                        </> :
                         <Message info>
                             <p className="center-text">No Parents available, Please create..</p>
                         </Message>}
